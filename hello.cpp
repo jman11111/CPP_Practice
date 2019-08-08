0f62668fe4247e34a184adbab32b477198dd6ba8
Jack <jbroncato@luc.edu>
2019-08-08 18:05:57 -0500

    queue and delimmiting string intake from file

--- hello.cpp
+++ hello.cpp
@@ -2,20 +2,32 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <queue>
 
 using namespace std;
 
 int main() {
     string pracArray[100];
     string currline;
+
+    queue <string> wordholder;
+
     ifstream testfile ("test.txt");
     if (testfile.is_open())
     {
-        while (getline(testfile,currline))
+        while (getline(testfile,currline,' '))
         {
-            
+            wordholder.push(currline);
+            //cout << currline << "\n";
         }
         testfile.close();
+
+        int qsize = wordholder.size();
+
+        for(int i = 0;i<qsize;i++){
+            cout << wordholder.front() << "\n";
+            wordholder.pop();
+        }
     }
 
     else cout << "Unable to open file";
